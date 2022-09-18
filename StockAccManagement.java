d0cfcc7bb5a0ddce0ca928b94f5b60563a4cf0fc
manasi025 <109495431+manasi025@users.noreply.github.com>
2022-09-18 14:17:02 +0530

    Update StockAccManagement.java

--- StockAccManagement.java
+++ StockAccManagement.java
@@ -9,15 +9,15 @@ public class StockAccManagement
 		// TODO Auto-generated method stub
 
 		Scanner in = new Scanner(System.in);
-        System.out.print("Enter Number of Stocks : ");
-        int numOfStocks = in.nextInt();
-        String[][] arr = new String [numOfStocks][3];
-        for(int i = 0; i < numOfStocks; i++)
-        {
-            for(int j = 0 ; j < 3 ; j++)
-            {
-                arr[i][j]=in.nextLine();
-            }
-        }
-     }
+        	System.out.print("Enter Number of Stocks : ");
+		int numOfStocks = in.nextInt();
+		String[][] arr = new String [numOfStocks][3];
+		for(int i = 0; i < numOfStocks; i++)
+		{
+		    for(int j = 0 ; j < 3 ; j++)
+		    {
+			arr[i][j]=in.nextLine();
+		    }
+        	}
+     	}
 }
